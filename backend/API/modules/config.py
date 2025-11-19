169303ad9884c9289c766441b1d80a4d681367e8
Javier Uribe <javieruribe00@outlook.com>
2025-11-19 16:52:17 -0700

    cambiar conexion a base de datos para servidor en la nube y local

--- backend/API/modules/config.py
+++ backend/API/modules/config.py
@@ -1,15 +1,20 @@
 import os 
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_USER = os.getenv("DB_USER", "admin")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "admin123")
-DB_HOST = os.getenv("DB_HOST", "db")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "placas_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+if not DATABASE_URL:
+    LOCAL_USER = os.getenv("DB_USER", "admin")
+    LOCAL_PASSWORD = os.getenv("DB_PASSWORD", "admin123")
+    LOCAL_HOST = os.getenv("DB_HOST", "db")
+    LOCAL_PORT = os.getenv("DB_PORT", "5432")
+    LOCAL_NAME = os.getenv("DB_NAME", "placas_db")
+
+    DATABASE_URL = f"postgresql+psycopg2://{LOCAL_USER}:{LOCAL_PASSWORD}@{LOCAL_HOST}:{LOCAL_PORT}/{LOCAL_NAME}"
+else:
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
 
 engine = create_engine(DATABASE_URL, echo=False, future=True)
 
